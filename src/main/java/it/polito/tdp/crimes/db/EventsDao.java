038397e0445d4b068f9b778b0d6b7953fcb7ecc5
Matteo Villosio <s258803@studenti.polito.it>
2022-05-24 11:13:40 +0200

    Versione finale (no controller)

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import it.polito.tdp.crimes.model.Adiacenza;
 import it.polito.tdp.crimes.model.Event;
 
 
@@ -52,6 +54,83 @@ public class EventsDao {
 			e.printStackTrace();
 			return null ;
 		}
+	}
+	
+	
+	public List<String> getVertici(String categoria, int mese){
+		
+		String sql= "SELECT DISTINCT offense_type_id "
+				+ "FROM events "
+				+ "WHERE offense_category_id = ? "
+				+ "AND MONTH(reported_date) = ?";
+		
+		List<String> vertici = new ArrayList<String>();
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				vertici.add(res.getString("offense_type_id"));
+				
+			}
+			
+			conn.close();
+			return vertici ;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	
+	
+	public List<Adiacenza> getArchi(String categoria, int mese){
+		
+		String sql="SELECT e1.offense_type_id as v1, e2.offense_type_id as v2, COUNT(DISTINCT e1.neighborhood_id) as peso "
+				+ "FROM events e1, events e2 "
+				+ "WHERE e1.offense_type_id > e2.offense_type_id "
+				+ "AND e1.offense_category_id = ? "
+				+ "AND e1.offense_category_id = e2.offense_category_id "
+				+ "AND MONTH(e1.reported_date) = ? "
+				+ "AND MONTH(e1.reported_date) = MONTH(e2.reported_date) "
+				+ "AND e1.neighborhood_id = e2.neighborhood_id "
+				+ "GROUP BY e1.offense_type_id, e2.offense_type_id ";
+		
+		
+		List<Adiacenza> archi = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				archi.add(new Adiacenza (res.getString("v1"),res.getString("v2"),res.getInt("peso")));
+			}
+			
+			conn.close();
+			return archi;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+		
 	}
 
 }
--- src/main/java/it/polito/tdp/crimes/model/TestModel.java
+++ src/main/java/it/polito/tdp/crimes/model/TestModel.java
@@ -4,6 +4,11 @@ public class TestModel {
 
 	public static void main(String[] args) {
 		
+		Model model = new Model();
+		model.creaGrafo("drug-alcohol", 2);
+		
+		System.out.println(model.getArchiMaggioriPesoMedio());
+		
 	}
 
 }
